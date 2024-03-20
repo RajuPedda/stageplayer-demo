26a9e4afb5028cd09703ba3385d99ce99b5dcd42
RajuPedda <raj.virtuali@gmail.com>
2024-03-20 15:23:20 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import singleSpaReact from 'single-spa-react';
+
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+});
+ 
+export const bootstrap = reactLifecycles.bootstrap;
+export const mount = reactLifecycles.mount;
+export const unmount = reactLifecycles.unmount;
+ 
+// If you're using single-spa layout, export a function to determine if the app should load
+export function isActive(location) {
+  console.log("---------")
+  return true; // Or implement custom logic
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
